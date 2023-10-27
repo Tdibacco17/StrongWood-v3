285e744f463ef11fd15e35c1eab825a9722fd86b
Tdibacco17 <tomas.dibacco@25watts.com.ar>
2023-10-27 09:16:18 -0300

    fix | opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,16 +16,16 @@ export const metadata: Metadata = {
     url: 'https://www.linkedin.com/in/tomas-di-bacco/'
   },
   manifest: '/manifest.json',
-  openGraph: {
-    images: {
-      url: 'https://strongwood-v3.vercel.app/opengraph.png',
-      secureUrl: 'https://strongwood-v3.vercel.app/opengraph.png',
-    },
-    title: 'StrongWood®',
-    siteName: 'StrongWood®',
-    description: 'El balance perfecto entre hierro y madera.',
-    url: 'https://strongwood-v3.vercel.app/',
-  }
+  // openGraph: {
+  //   images: {
+  //     url: 'https://strongwood-v3.vercel.app/opengraph.png',
+  //     secureUrl: 'https://strongwood-v3.vercel.app/opengraph.png',
+  //   },
+  //   title: 'StrongWood®',
+  //   siteName: 'StrongWood®',
+  //   description: 'El balance perfecto entre hierro y madera.',
+  //   url: 'https://strongwood-v3.vercel.app/',
+  // }
 }
 
 export default function RootLayout({
